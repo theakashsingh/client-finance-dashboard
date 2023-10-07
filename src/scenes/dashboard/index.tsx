92cd4013b3d51f0383bf019e89de7f48898440f4
Akash Singh <mr.singhaksh@gmail.com>
2023-10-08 03:47:37 +0530

    feat:Implement redux for backend end point

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,54 +1,13 @@
-import DashboardBox from "@/components/DashboardBox";
-import { Box, useMediaQuery, useTheme } from "@mui/material";
-import React from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import {
+  gridTemplateLargeScreens,
+  gridTemplateSmallScreens,
+} from "./dashboardGridArea";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
-type Props = {};
-
-const gridTemplateLargeScreens = `
-"a b c"
-"a b c"
-"a b c"
-"a b f"
-"d e f"
-"d e f"
-"d g h"
-"i g h"
-"i g j"
-"i g j"
-`;
-const gridTemplateSmallScreens = `
-"a"
-"a"
-"a"
-"a"
-"b"
-"b"
-"b"
-"b"
-"c"
-"c"
-"c"
-"d"
-"d"
-"d"
-"e"
-"e"
-"f"
-"f"
-"f"
-"g"
-"g"
-"g"
-"g"
-"h"
-"h"
-"i"
-"i"
-"i"
-"j"
-"j"
-`;
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1200px)");
   return (
     <Box
@@ -56,27 +15,23 @@ const Dashboard = (props: Props) => {
       height="100%"
       display="grid"
       gap="1.5rem"
-      sx={ isAboveMediumScreens ? {
-        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-        gridTemplateRows: "repeat(10,minmax(60px, 1fr))",
-        gridTemplateAreas: gridTemplateLargeScreens,
-      } : {
-        gridAutoColumns: "1fr",
-        gridAutoRows: "80px",
-        gridTemplateAreas:gridTemplateSmallScreens
-      }}
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10,minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
     >
-       
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
-      <DashboardBox gridArea="d"></DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
-      <DashboardBox gridArea="f"></DashboardBox>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <Row1/>
+      <Row2/>
+      <Row3/>
     </Box>
   );
 };
